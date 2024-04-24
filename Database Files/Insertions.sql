c872f9ba7610282609829336eaa3f280a3074aa8
Martin Valentine <nguythequang200@gmail.com>
2024-04-24 07:07:03 +0700

    Adjust sql

--- Database Files/Insertions.sql
+++ Database Files/Insertions.sql
@@ -27,65 +27,60 @@ insert into department values(5 , 'Neurology', 'A medical speciality dealing wit
 
 
 --LOGIN TABLE INSERTIION
-INSERT INTO LoginTable(Email, Password, Type) VALUES('farhan@gmail.com', 'abc', 2)
-INSERT INTO LoginTable(Email, Password, Type) VALUES('kashan@gmail.com', 'abc', 2)
-INSERT INTO LoginTable(Email, Password, Type) VALUES('hassaan@gmail.com', 'abc',2)
-INSERT INTO LoginTable(Email, Password, Type) VALUES('haris@gmail.com', 'abc', 2)
-INSERT INTO LoginTable(Email, Password, Type) VALUES('alvi@gmail.com', 'abc', 2)
-INSERT INTO LoginTable(Email, Password, Type) VALUES('talha@gmail.com', 'abc', 2)
-INSERT INTO LoginTable(Email, Password, Type) VALUES('shariq@gmail.com', 'abc', 2)
-INSERT INTO LoginTable(Email, Password, Type) VALUES('awais@gmail.com', 'abc', 2)
-INSERT INTO LoginTable(Email, Password, Type) VALUES('saifi@gmail.com', 'abc', 2)
-INSERT INTO LoginTable(Email, Password, Type) VALUES('mansha@gmail.com', 'abc', 2)
+INSERT INTO LoginTable(Email, Password, Type) VALUES('trung@gmail.com', 'abc', 2)
+INSERT INTO LoginTable(Email, Password, Type) VALUES('phuong@gmail.com', 'abc', 2)
+INSERT INTO LoginTable(Email, Password, Type) VALUES('linh@gmail.com', 'abc', 2)
+INSERT INTO LoginTable(Email, Password, Type) VALUES('minh@gmail.com', 'abc', 2)
+INSERT INTO LoginTable(Email, Password, Type) VALUES('anh@gmail.com', 'abc', 2)
+INSERT INTO LoginTable(Email, Password, Type) VALUES('tuan@gmail.com', 'abc', 2)
+INSERT INTO LoginTable(Email, Password, Type) VALUES('duc@gmail.com', 'abc', 2)
+INSERT INTO LoginTable(Email, Password, Type) VALUES('hanh@gmail.com', 'abc', 2)
+INSERT INTO LoginTable(Email, Password, Type) VALUES('thuy@gmail.com', 'abc', 2)
+INSERT INTO LoginTable(Email, Password, Type) VALUES('vu@gmail.com', 'abc', 2)
 
-INSERT INTO LoginTable(Email, Password, Type) VALUES('ABC@gmail.com', 'abc', 1)
-INSERT INTO LoginTable(Email, Password, Type) VALUES('DEF@gmail.com', 'abc', 1)
-INSERT INTO LoginTable(Email, Password, Type) VALUES('XYZ@gmail.com', 'abc', 1)
-
-
+INSERT INTO LoginTable(Email, Password, Type) VALUES('thanh@gmail.com', 'abc', 1)
+INSERT INTO LoginTable(Email, Password, Type) VALUES('hoang@gmail.com', 'abc', 1)
+INSERT INTO LoginTable(Email, Password, Type) VALUES('hung@gmail.com', 'abc', 1)
 
 --DOCTOR INSERTIONS
 DECLARE @ID INT
-SELECT @ID = LoginID FROM LoginTable WHERE Email = 'farhan@gmail.com'
-INSERT INTO Doctor VALUES(@ID, 'Farhan Shoukat', '156133213', 'Enjoy, Lahore', '4-12-1996', 'M', 1, 2500, 30000, 4, 0, 'PHD IN EVERY FIELD KNOWN TO MAN', 'ENJOY', 10, 1)
-SELECT @ID = LoginID FROM LoginTable WHERE Email = 'kashan@gmail.com'
-INSERT INTO Doctor VALUES(@ID, 'KASHAN', '156133213', 'Enjoy, Lahore', '12-12-1996', 'M', 1, 3000, 25000, 3.5, 0, 'PHD IN EVERY FIELD KNOWN TO MAN', 'ENJOY', 10, 1)
-SELECT @ID = LoginID FROM LoginTable WHERE Email = 'hassaan@gmail.com'
-INSERT INTO Doctor VALUES(@ID, 'HASSAAN', '156133213', 'Enjoy, Lahore', '12-12-1996', 'M', 1, 1500, 20000, 5, 0, 'PHD IN EVERY FIELD KNOWN TO MAN', 'ENJOY', 10, 1)
-SELECT @ID = LoginID FROM LoginTable WHERE Email = 'haris@gmail.com'
-INSERT INTO Doctor VALUES(@ID, 'HARIS MUNEER', '156133213', 'Enjoy, Lahore', '05-04-1990', 'M', 1, 1000, 15000, 4.5, 0, 'PHD IN EVERY FIELD KNOWN TO MAN', 'ENJOY', 10, 1)
-SELECT @ID = LoginID FROM LoginTable WHERE Email = 'talha@gmail.com'
-INSERT INTO Doctor VALUES(@ID, 'Talha MUNEER', '156133213', 'Enjoy, Lahore', '05-04-1990', 'M', 2, 1000, 15000, 4.5, 0, 'PHD IN EVERY FIELD KNOWN TO MAN', 'ENJOY', 10, 1)
-SELECT @ID = LoginID FROM LoginTable WHERE Email = 'shariq@gmail.com'
-INSERT INTO Doctor VALUES(@ID, 'Shariq MUNEER', '156133213', 'Enjoy, Lahore', '05-04-1990', 'M', 2, 1000, 15000, 4.5, 0, 'PHD IN EVERY FIELD KNOWN TO MAN', 'ENJOY', 10, 1)
-SELECT @ID = LoginID FROM LoginTable WHERE Email = 'awais@gmail.com'
-INSERT INTO Doctor VALUES(@ID, 'Awais MUNEER', '156133213', 'Enjoy, Lahore', '05-04-1990', 'M', 3, 1000, 15000, 4.5, 0, 'PHD IN EVERY FIELD KNOWN TO MAN', 'ENJOY', 10, 1)
-SELECT @ID = LoginID FROM LoginTable WHERE Email = 'alvi@gmail.com'
-INSERT INTO Doctor VALUES(@ID, 'Alvi', '156133213', 'Enjoy, Lahore', '05-04-1990', 'M', 3, 1000, 15000, 4.5, 0, 'PHD IN EVERY FIELD KNOWN TO MAN', 'ENJOY', 10, 1)
-SELECT @ID = LoginID FROM LoginTable WHERE Email = 'saifi@gmail.com'
-INSERT INTO Doctor VALUES(@ID, 'Saifi', '156133213', 'Enjoy, Lahore', '05-04-1990', 'M', 4, 1000, 15000, 4.5, 0, 'PHD IN EVERY FIELD KNOWN TO MAN', 'ENJOY', 10, 1)
-SELECT @ID = LoginID FROM LoginTable WHERE Email = 'mansha@gmail.com'
-INSERT INTO Doctor VALUES(@ID, 'Mansha', '156133213', 'Enjoy, Lahore', '05-04-1990', 'M', 5, 1000, 15000, 4.5, 0, 'PHD IN EVERY FIELD KNOWN TO MAN', 'ENJOY', 10, 1)
-
+SELECT @ID = LoginID FROM LoginTable WHERE Email = 'trung@gmail.com'
+INSERT INTO Doctor VALUES(@ID, 'Trung Nguyen', '156133213', 'Enjoy, Hanoi', '4-12-1996', 'M', 1, 2500, 30000, 4, 0, 'PHD IN EVERY FIELD KNOWN TO MAN', 'ENJOY', 10, 1)
+SELECT @ID = LoginID FROM LoginTable WHERE Email = 'phuong@gmail.com'
+INSERT INTO Doctor VALUES(@ID, 'Phuong Tran', '156133213', 'Enjoy, Hanoi', '12-12-1996', 'M', 1, 3000, 25000, 3.5, 0, 'PHD IN EVERY FIELD KNOWN TO MAN', 'ENJOY', 10, 1)
+SELECT @ID = LoginID FROM LoginTable WHERE Email = 'linh@gmail.com'
+INSERT INTO Doctor VALUES(@ID, 'Linh Nguyen', '156133213', 'Enjoy, Hanoi', '12-12-1996', 'M', 1, 1500, 20000, 5, 0, 'PHD IN EVERY FIELD KNOWN TO MAN', 'ENJOY', 10, 1)
+SELECT @ID = LoginID FROM LoginTable WHERE Email = 'minh@gmail.com'
+INSERT INTO Doctor VALUES(@ID, 'Minh Nguyen', '156133213', 'Enjoy, Hanoi', '05-04-1990', 'M', 1, 1000, 15000, 4.5, 0, 'PHD IN EVERY FIELD KNOWN TO MAN', 'ENJOY', 10, 1)
+SELECT @ID = LoginID FROM LoginTable WHERE Email = 'tuan@gmail.com'
+INSERT INTO Doctor VALUES(@ID, 'Tuan Le', '156133213', 'Enjoy, Hanoi', '05-04-1990', 'M', 2, 1000, 15000, 4.5, 0, 'PHD IN EVERY FIELD KNOWN TO MAN', 'ENJOY', 10, 1)
+SELECT @ID = LoginID FROM LoginTable WHERE Email = 'duc@gmail.com'
+INSERT INTO Doctor VALUES(@ID, 'Duc Pham', '156133213', 'Enjoy, Hanoi', '05-04-1990', 'M', 2, 1000, 15000, 4.5, 0, 'PHD IN EVERY FIELD KNOWN TO MAN', 'ENJOY', 10, 1)
+SELECT @ID = LoginID FROM LoginTable WHERE Email = 'hanh@gmail.com'
+INSERT INTO Doctor VALUES(@ID, 'Hanh Vu', '156133213', 'Enjoy, Hanoi', '05-04-1990', 'M', 3, 1000, 15000, 4.5, 0, 'PHD IN EVERY FIELD KNOWN TO MAN', 'ENJOY', 10, 1)
+SELECT @ID = LoginID FROM LoginTable WHERE Email = 'thuy@gmail.com'
+INSERT INTO Doctor VALUES(@ID, 'Thuy Nguyen', '156133213', 'Enjoy, Hanoi', '05-04-1990', 'M', 3, 1000, 15000, 4.5, 0, 'PHD IN EVERY FIELD KNOWN TO MAN', 'ENJOY', 10, 1)
+SELECT @ID = LoginID FROM LoginTable WHERE Email = 'vu@gmail.com'
+INSERT INTO Doctor VALUES(@ID, 'Vu Tran', '156133213', 'Enjoy, Hanoi', '05-04-1990', 'M', 4, 1000, 15000, 4.5, 0, 'PHD IN EVERY FIELD KNOWN TO MAN', 'ENJOY', 10, 1)
 
 --PATIENT INSERTIONS
 DECLARE @P_ID INT
-SELECT @P_ID = LoginID FROM LoginTable WHERE Email='ABC@gmail.com'
-INSERT INTO Patient VALUES(@P_ID, 'ABC', '61536516', 'ENJOY, LAHORE', '4-4-1996', 'M')
-SELECT @P_ID = LoginID FROM LoginTable WHERE Email='DEF@gmail.com'
-INSERT INTO Patient VALUES(@P_ID, 'DEF', '61536516', 'ENJOY, LAHORE', '4-4-1996', 'M')
-SELECT @P_ID = LoginID FROM LoginTable WHERE Email='XYZ@gmail.com'
-INSERT INTO Patient VALUES(@P_ID, 'XYZ', '61536516', 'ENJOY, LAHORE', '4-4-1996', 'M')
-
+SELECT @P_ID = LoginID FROM LoginTable WHERE Email='thanh@gmail.com'
+INSERT INTO Patient VALUES(@P_ID, 'Thanh', '61536516', 'ENJOY, HANOI', '4-4-1996', 'M')
+SELECT @P_ID = LoginID FROM LoginTable WHERE Email='hoang@gmail.com'
+INSERT INTO Patient VALUES(@P_ID, 'Hoang', '61536516', 'ENJOY, HANOI', '4-4-1996', 'M')
+SELECT @P_ID = LoginID FROM LoginTable WHERE Email='hung@gmail.com'
+INSERT INTO Patient VALUES(@P_ID, 'Hung', '61536516', 'ENJOY, HANOI', '4-4-1996', 'M')
 
 --select * from OtherStaff
 
-insert into OtherStaff values ('Javed', '03227561002','Iqbal Town, Lhr', 'Guard', 'M', '05-04-1990', 'Matric',5000)
-insert into OtherStaff values ('Hamza', '03227561002','Iqbal Town, Lhr', 'Sweeper', 'M', '05-04-1990', 'Matric',5000)
-insert into OtherStaff values ('Kashan', '03227561002','Iqbal Town, Lhr', 'Security', 'M', '05-04-1990', 'Matric',5000)
-insert into OtherStaff values ('Alio', '03227561002','Iqbal Town, Lhr', 'Guard', 'M', '05-04-1990', 'Matric',5000)
-insert into OtherStaff values ('Kaleem', '03227561002','Iqbal Town, Lhr', 'Guard', 'M', '05-04-1990', 'Matric',5000)
-insert into OtherStaff values ('Ali', '03227561002','Iqbal Town, Lhr', 'Guard', 'M', '05-04-1990', 'Matric',5000)
+insert into OtherStaff values ('Bao', '03227561002','Hoan Kiem, Hanoi', 'Guard', 'M', '05-04-1990', 'Matric',5000)
+insert into OtherStaff values ('Trong', '03227561002','Hoan Kiem, Hanoi', 'Sweeper', 'M', '05-04-1990', 'Matric',5000)
+insert into OtherStaff values ('Lan', '03227561002','Hoan Kiem, Hanoi', 'Security', 'M', '05-04-1990', 'Matric',5000)
+insert into OtherStaff values ('Anh', '03227561002','Hoan Kiem, Hanoi', 'Guard', 'M', '05-04-1990', 'Matric',5000)
+insert into OtherStaff values ('Kieu', '03227561002','Hoan Kiem, Hanoi', 'Guard', 'M', '05-04-1990', 'Matric',5000)
+insert into OtherStaff values ('Huong', '03227561002','Hoan Kiem, Hanoi', 'Guard', 'M', '05-04-1990', 'Matric',5000)
+
 
 
 
